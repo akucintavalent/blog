7b4cd062bd4e6c129440d6e21b78f114a878dfae
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-03-24 12:02:59 +0100

    Fix linter errors

--- Gemfile
+++ Gemfile
@@ -76,7 +76,6 @@ gem 'rubocop', '>= 1.0', '< 2.0'
 # Run against this stable release
 group :development, :test do
   gem 'rspec-rails', '~> 5.0.0'
-  gem 'capybara'
 end
 
 gem 'rails-controller-testing'
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,38 +1,38 @@
 require 'rails_helper'
 require 'capybara/rspec'
 
-RSpec.describe "Users", type: :request do
-  context "testing responce" do
-    it "GET /users" do
+RSpec.describe 'Users', type: :request do
+  context 'testing responce' do
+    it 'GET /users' do
       get('/users')
-      expect(response).to render_template('index') 
+      expect(response).to render_template('index')
     end
 
-    it "GET /users/:user_id" do
+    it 'GET /users/:user_id' do
       get('/users/3')
-      expect(response).to render_template('show') 
+      expect(response).to render_template('show')
     end
 
-    it "GET /users status 200" do
+    it 'GET /users status 200' do
       get('/users')
-      expect(response).to have_http_status(:ok) 
+      expect(response).to have_http_status(:ok)
     end
 
-    it "GET /users/:user_id/posts/:id status 200" do
+    it 'GET /users/:user_id/posts/:id status 200' do
       get('/users/3')
-      expect(response).to have_http_status(:ok) 
+      expect(response).to have_http_status(:ok)
     end
   end
 end
 
-RSpec.describe "Users", type: :feature do
-  context "testing with capybara" do
-    it "visit /users" do
+RSpec.describe 'Users', type: :feature do
+  context 'testing with capybara' do
+    it 'visit /users' do
       visit '/users'
       expect(page).to have_text('Here is a list users')
     end
 
-    it "visit /users/:user_id" do
+    it 'visit /users/:user_id' do
       visit '/users/3'
       expect(page).to have_text('Here is a given user\'s page')
     end
