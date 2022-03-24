51ef4ac6d25b2533ea52af633f55553c59d496c8
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-03-24 21:20:43 +0100

    Change the types of columns from string to text

--- db/migrate/20220324163932_create_users.rb
+++ db/migrate/20220324163932_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :photo
-      t.string :bio
+      t.text :name
+      t.text :photo
+      t.text :bio
       t.integer :posts_counter
 
       t.timestamps
--- db/migrate/20220324164502_create_posts.rb
+++ db/migrate/20220324164502_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.string :text
+      t.text :title
+      t.text :text
       t.integer :comments_counter
       t.integer :likes_counter
 
