073e68de85cfd6b452159ac1ad687589e393809d
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-03-24 22:56:28 +0100

    Fix the datatypes

--- db/migrate/20220324163932_create_users.rb
+++ db/migrate/20220324163932_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.text :name
-      t.text :photo
+      t.string :name
+      t.string :photo
       t.text :bio
       t.integer :posts_counter
 
