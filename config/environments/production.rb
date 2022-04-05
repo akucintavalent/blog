f3705a711cc423921a5115c322fd692dc575d7b0
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-04-05 10:28:14 +0200

    Managed to be able to send actual emails when reistering

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  host = 'localhost:3000' #replace with your own url
+  config.action_mailer.default_url_options = { host: host }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => <gmail_username>,
+    :password             => <gmail_password>,
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
