5bd9d6a52d6d1157b831a0013aa0e974f091f13c
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-04-07 18:10:07 +0200

    Create Users show view tests 2

--- spec/views/users_show_spec.rb
+++ spec/views/users_show_spec.rb
@@ -44,7 +44,6 @@ RSpec.describe 'Users index page', type: :system do
       )
     ]
   end
-  
 
   it 'checks if there is a profile image' do
     visit '/users/sign_in'
@@ -132,34 +131,4 @@ RSpec.describe 'Users index page', type: :system do
     click_link 'See all posts'
     expect(page).to have_current_path("/users/#{user.id}/posts")
   end
-
-  # it 'checks if every user is has a profile picture' do
-  #   visit '/users/sign_in'
-  #   fill_in 'user_email', with: users[0].email
-  #   fill_in 'user_password', with: users[0].password
-  #   click_button 'Log in'
-    
-  #   expect(page.all(:css, 'img.squared-pic').length).to be users.length
-  # end
-
-  # it 'checks if we see the correct number of posts' do
-  #   visit '/users/sign_in'
-  #   fill_in 'user_email', with: users[0].email
-  #   fill_in 'user_password', with: users[0].password
-  #   click_button 'Log in'
-    
-  #   users.each do |user|
-  #     expect(page).to have_content("Number of posts: #{user.posts_counter}")
-  #   end
-  # end
-
-  # it 'checks if we go to the users page after clicking on their image' do
-  #   visit '/users/sign_in'
-  #   fill_in 'user_email', with: users[2].email
-  #   fill_in 'user_password', with: users[2].password
-  #   click_button 'Log in'
-    
-  #   page.all(:css, 'img.squared-pic')[0].click()
-  #   expect(page).to have_current_path("/users/#{users[0].id}")
-  # end
 end
