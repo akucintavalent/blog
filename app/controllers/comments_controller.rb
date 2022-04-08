86cf5b37c204962037264e7aa48ebfd37132eec8
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-04-08 19:53:19 +0200

    Modify application controller & Implement adding comments and viewing them

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,8 @@
 class CommentsController < ApplicationController
+  skip_before_action :authenticate_request
+  before_action :authenticate_request, only: [:add_comment]
+  protect_from_forgery with: :null_session, only: [:add_comment]
+
   def create
     @comment = current_user.comments.new(comment_params)
 
@@ -18,6 +22,26 @@ class CommentsController < ApplicationController
     comment.destroy
     flash[:notice] = 'Comment was successfully removed'
     redirect_to request.path
+  end
+
+  def get_comments
+    post = User.find(params[:user_id]).posts.find(params[:post_id])
+
+    respond_to do |format|
+      format.json { render json: post.comments }
+    end
+  end
+
+  def add_comment
+    comment = Comment.new(author: @curr_user, post_id: params[:post_id], text: params[:text])
+
+    respond_to do |format|
+      if comment.save
+        format.json { render json: comment }
+      else
+        format.json { render json: { :success => false, :message => comment.errors.full_messages} }
+      end
+    end
   end
 
   private
