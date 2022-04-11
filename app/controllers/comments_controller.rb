7c6303cf9cfc78982396c209606b8234e1526684
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-04-11 19:09:54 +0200

    Fix comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,34 +25,48 @@ class CommentsController < ApplicationController
   end
 
   # rubocop:disable Naming/AccessorMethodName
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/PerceivedComplexity
 
   def get_comments
-    user = User.where(id: params[:user_id])[0]
+    user_id = nil
+    post_id = nil
+    params.each_pair do |key, value|
+      next unless value.nil?
+
+      json_key = JSON.parse(key)
+      user_id = json_key['user_id']
+      post_id = json_key['post_id']
+    end
+
+    user = User.where(id: user_id || params[:user_id])[0]
 
     respond_to do |format|
       if user
-        post = user.posts.where(id: params[:post_id])[0]
+        post = user.posts.where(id: post_id || params[:post_id])[0]
         if post
           format.json { render json: post.comments }
         else
-          format.json { render json: { success: false, message: ['Post must exist'] } }
+          format.json { render json: { success: false, message: ['Post must exist'] }, status: 404 }
         end
       else
-        format.json { render json: { success: false, message: ['User must exist'] } }
+        format.json { render json: { success: false, message: ['User must exist'] }, status: 404 }
       end
     end
   end
 
+  # rubocop:enable Metrics/MethodLength
   # rubocop:enable Naming/AccessorMethodName
+  # rubocop:enable Metrics/PerceivedComplexity
 
   def add_comment
     comment = Comment.new(author: @curr_user, post_id: params[:post_id], text: params[:text])
 
     respond_to do |format|
       if comment.save
-        format.json { render json: comment }
+        format.json { render json: comment, status: 201 }
       else
-        format.json { render json: { success: false, message: comment.errors.full_messages } }
+        format.json { render json: { success: false, message: comment.errors.full_messages }, status: 422 }
       end
     end
   end
